2e74783db7b1dfa2475faf05fe38750c43ff0675
北川弘貴 <hiroki.k@kitagawahiroshitakanoMacBook-puro.local>
2018-01-16 21:35:42 +0900

    Finish4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,7 +20,16 @@ class BlogsController < ApplicationController
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blogs_path(blog)
+  end
+
+
 
   private
   	def blog_params
